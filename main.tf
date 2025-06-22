987a4e6a16612f70212d408f759db9ea1fdd8524
manik007 <manik007@gmail.com>
2025-06-22 18:31:09 -0400

    syntax error for http listener

--- main.tf
+++ main.tf
@@ -68,14 +68,14 @@ module "alb" {
     }
   }
 
-  http_tcp_listeners = [
+  listeners = [
     {
       port                = 80
       protocol            = "HTTP"
       target_group_index  = 0
     }
   ]
-  
+
   tags = {
     Environment = "dev"
   }
