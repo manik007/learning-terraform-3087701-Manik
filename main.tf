c0440d1b15577862d48781a70e8a9cf9ccc41c0d
manik007 <manik007@gmail.com>
2025-06-22 23:18:54 -0400

    NO auto scaling

--- main.tf
+++ main.tf
@@ -34,22 +34,17 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  #version = "8.3.0"
+resources "aws_instance" "blog" {
+  ami = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  desired_capacity = 1
-
-  vpc_zone_identifier   = module.blog_vpc.public_subnets
- #target_group_arns     = module.blog_alb.target_group_arns
-  security_groups       = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   
-  image_id              = data.aws_ami.app_ami.id
-  instance_type         = var.instance_type
+  tags = {
+    Name = "HelloWorld" 
+  }
+
 }
 
 module "blog_alb" {
@@ -70,6 +65,10 @@ module "blog_alb" {
       backend_protocol = "HTTP" 
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        target_id = aws_instance.blog.id
+        port = 80
+      }
     }
   ]
 
