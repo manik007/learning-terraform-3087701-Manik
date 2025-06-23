c90c9ce449886dcdfbef1c8463a5661258a917f8
manik007 <manik007@gmail.com>
2025-06-22 22:37:56 -0400

    auto scaling issue fix

--- main.tf
+++ main.tf
@@ -34,22 +34,20 @@ module "blog_vpc" {
   }
 }
 
-
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
-  # insert the 1 required variable here
-
+ 
   name = "blog"
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns  = module.blog_alb.target_group_arns
-  security_groups      = [module.blog_sg.security_group_id]
+  vpc_zone_identifier   = module.blog_vpc.public_subnets
+  target_group_arns     = module.blog_alb.target_group_arns
+  security_groups       = [module.blog_sg.security_group_id]
   
-  image_id            = data.aws_ami.app_ami.id
-  instance_type       = var.instance_type
+  image_id              = data.aws_ami.app_ami.id
+  instance_type         = var.instance_type
 }
 
 module "blog_alb" {
@@ -99,4 +97,3 @@ module "blog_sg" {
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
